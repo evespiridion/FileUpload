2c5d19337acdcfbaae9f3cfff2120e1945d911fc
evespiridion <eric.espiridion@gmail.com>
2024-10-17 16:38:18 +0800

    Add project files.

---
+++ FileUpload.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FileUpload.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
